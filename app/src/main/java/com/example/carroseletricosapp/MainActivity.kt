c23a4c44c7eeadec0a6efc57e98ec755e194dcd0
aline <alinecarvalho.pro@gmail.com>
2023-08-29 12:37:44 -0300

    feat: Captura e cáculo de valores

--- app/src/main/java/com/example/carroseletricosapp/MainActivity.kt
+++ app/src/main/java/com/example/carroseletricosapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.carroseletricosapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var precokwh: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configurarViews()
+        configurarListeners()
+    }
+
+    fun configurarViews() {
+        precokwh = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        btnCalcular = findViewById(R.id.btn_calcular)
+        resultado = findViewById(R.id.tv_resultado)
+    }
+
+    fun configurarListeners() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    fun calcular() {
+        val preco = precokwh.text.toString().toFloat()
+        val km = kmPercorrido.text.toString() .toFloat()
+        val calculo = preco / km
+        resultado.text = calculo.toString()
     }
 }
